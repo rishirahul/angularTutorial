e09c91f2cad0864b07c3f8c8f9a9b541ff20e2b2
rishirahul <rishi.iitkharagpur@gmail.com>
2018-12-27 00:06:01 +0530

    template component using ng content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IopropertyComponent } from './ioproperty/ioproperty.component';
+import { NgcontentComponent } from './ngcontent/ngcontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IopropertyComponent
+    IopropertyComponent,
+    NgcontentComponent
   ],
   imports: [
     BrowserModule,
